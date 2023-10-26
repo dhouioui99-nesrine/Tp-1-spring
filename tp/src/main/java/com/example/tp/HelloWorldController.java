030c32001fb21079d7fdb61af2bc150bb2535bde
Dhouioui nesrine <58806683+dhouioui99-nesrine@users.noreply.github.com>
2023-10-26 10:09:59 +0100

    Add files via upload

---
+++ tp/src/main/java/com/example/tp/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.example.tp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloWorldController {
+// GET HTTP Method
+// http://localhost:8089/hello-world
+@GetMapping("/hello-world")
+public String helloWorld() {
+return "Hello World!";
+}
+}
